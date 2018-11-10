445b39c5d08d7dbd687a1c9744e57c313572e066
Mir Farid <mirfarid7160@gmail.com>
2018-11-10 19:33:42 +0000

    google fonts

---
+++ javascript.js
@@ -0,0 +1,16 @@
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+    
+    var slides = document.getElementsByClassName("myimages");
+    //var slides = [img1, img2, img3]
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex>=slides.length) {
+        slideIndex=0;
+    }
+    slides[slideIndex].style.display = "block";
+    setTimeout(showSlides, 4000);
+}
